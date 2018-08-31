1301dbdb5d08d8c35012729687b354255cb97d32
Ilya <pistol88@gmail.com>
2018-08-31 12:57:56 +0300

    pow reduced

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -113,7 +113,7 @@ public:
 		//strSporkKey = "046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e";
 		//strMasternodePaymentsPubKey = "046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e";
 		strDarksendPoolDummyAddress = "CRaVnHZizkREBg6yBzcpy6TBLn4B5GbUva";
-		nLastPOWBlock = 1000000;
+		nLastPOWBlock = 50000;
 		nPOSStartBlock = 15000;
 	}
 
